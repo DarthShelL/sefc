9d2334f073ad60060ebab86869da5a31bf4bd93c
DarthShelL <shellnosferatu@gmail.com>
2025-04-01 23:34:40 +0300

    fork

--- ClientPlugin/Plugin.cs
+++ ClientPlugin/Plugin.cs
@@ -1,21 +1,32 @@
-﻿using System;
-using System.Reflection;
-using ClientPlugin.Settings;
+﻿using ClientPlugin.Settings;
 using ClientPlugin.Settings.Layouts;
 using HarmonyLib;
+using Sandbox.Game.Entities;
+using Sandbox.Game.Gui;
+using Sandbox.Game.World;
 using Sandbox.Graphics.GUI;
+using Sandbox.ModAPI;
+using SpaceEngineers.Game.World;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using VRage.GameServices;
 using VRage.Plugins;
+using VRageMath;
 
 namespace ClientPlugin
 {
     // ReSharper disable once UnusedType.Global
     public class Plugin : IPlugin, IDisposable
     {
-        public const string Name = "ClientPluginTemplate";
+        public const string Name = "DarthsPlugin";
         public static Plugin Instance { get; private set; }
         private SettingsGenerator settingsGenerator;
 
-        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
+        //[System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
         public void Init(object gameInstance)
         {
             Instance = this;
@@ -37,6 +48,22 @@ namespace ClientPlugin
         public void Update()
         {
             // TODO: Put your update code here. It is called on every simulation frame!
+
+            if (MySession.Static.LocalHumanPlayer.Controller.ControlledEntity != null)
+            {
+                MyCubeBlock myCubeBlock1 = MySession.Static.LocalHumanPlayer.Controller.ControlledEntity as MyCubeBlock;
+                MyCubeGrid MyGrid = myCubeBlock1.CubeGrid;
+
+                float baseMass;
+                float physicalMass;
+                MyGrid.GetCurrentMass(out baseMass, out physicalMass);
+                Vector3 velocity = MyGrid.GetPhysicsBody().LinearVelocity;
+
+
+                MyHud.Chat.ShowMessage("Darth's plugin", "FUCK!", Color.Red);
+            }
+            //MyGuiManager.DrawString("Debug", "Fuck", new VRageMath.Vector2(-0.5f, -0.5f), 1f, null, VRage.Utils.MyGuiDrawAlignEnum.HORISONTAL_LEFT_AND_VERTICAL_TOP, true);
+            //MyCubeGrid.
         }
 
         // ReSharper disable once UnusedMember.Global
